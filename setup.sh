b69ec7ea9b1b710759e2e2a9ffd1ed5ce8e972b3
Pawel Kurdybacha <kladku@gmail.com>
2016-07-23 14:37:52 +0100

    Git configuration questions

--- setup.sh
+++ setup.sh
@@ -19,6 +19,25 @@ main() {
     ask_for_sudo
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   
+    if ! $skipQuestions; then
+
+        print_info "Git"
+
+        ask_for_confirmation "Do you want to setup git user and email?"
+        printf "\n"
+
+        if answer_is_yes; then
+            ask "user.name: "
+            git config --global user.name "$(get_answer)"
+            ask "user.email: "
+            git config --global user.email "$(get_answer)"
+        fi
+
+    fi
+    
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     ./create_symbolic_links.sh
     ./ubuntu/install.sh
